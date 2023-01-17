cb3c82307da275933abb4a548a9ed72a2b619cdf
Irem Atak <irematak436@gmail.com>
2023-01-18 02:00:25 +0300

    Update AnaMenu.cs

--- Assets/scripts/AnaMenu.cs
+++ Assets/scripts/AnaMenu.cs
@@ -6,34 +6,10 @@ using UnityEngine.SceneManagement;
 public class AnaMenu: MonoBehaviour
 {
     
-    // oyunu yeniden başlatmak için 
+   
     public void reStart(){
-        // MEVCUT SAHNEYİ YUÜKLER:
-        //SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+        
         SceneManager.LoadSceneAsync("level1") ;
     }
-    /*
-    public void start(){
-        Button DevamButton = FindObjectOfType<Button>();
-        
-        ısSeeButton(); // true kayıtlı oyun var onu yükle button gözüksün
-        if(!ısSeeButton()) {
-            DevamButton.gameObject.SetActive(false); 
-        }else{
-            // true: kayıtlı oyunu getir
-            DevamButton.onClick.AddListener(DevamEt());
-        }
-    }
-     public void DevamEt(){ // buttona ver 
-        // button güzüküyor ve basılınca kayıtlı oyunu getir get
-        SceneManager.LoadSceneAsync("LEVEL2") ;
-
-     }
-     bool  ısSeeButton(){
-
-        // oyunun kayıtlı olup olmadıgını kontrol et
-       return true; 
-      
-    }
-     */
+   
 }
